36aabffdf96ecd68d664a267590cdbb1f2f838cb
Rohit Gupta <30778939+rohitgr7@users.noreply.github.com>
2017-10-19 00:00:50 +0530

    handeled auth

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport config
+require('./config/passport')(passport);
+
+// Load routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It works');
-})
+});
+
+app.use('/auth', auth);
 
 const port = process.env.PORT || 3000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongodbURI: 'mongodb://mark:qwerty123@ds035702.mlab.com:35702/storybooks-dev',
+  googleClientID: '685777261343-h1j5ec0l6rcqhtc37sealk0mmagtr8jd.apps.googleusercontent.com',
+  googleClientSecret: 'ja1pVY6FBH-MYgoVyP1Fk1H1'
+}
